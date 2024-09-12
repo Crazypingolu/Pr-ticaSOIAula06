262798579fcc727944a8799bab295fa8330272dd
FATEC ZONA LESTE <lucaswar05@gmail.com>
2024-09-12 16:40:47 -0300

    Exe 01 - view 1.0

--- Exercicio01/src/view/ClassePrincipal.java
+++ Exercicio01/src/view/ClassePrincipal.java
@@ -1,6 +1,24 @@
 package view;
+// Trazer Bibliotecas e Pacotes
+import java.util.concurrent.Semaphore;
+import controller.ClasseThreads;
+import java.util.Random;
+
 public class ClassePrincipal {
     public static void main(String[] args) {
-        
+        Random r = new Random();
+        Semaphore s = new Semaphore(1);
+        int[] por = new int[4];
+        int portaCerta = r.nextInt(3);
+        por[portaCerta] = 1;
+        int pedra = 0;
+        int tocha = 0;
+        int distancia = 2000;
+
+        for (int i = 0; i < 4; i++) {
+            ClasseThreads t = new ClasseThreads(i, s, pedra, tocha, distancia, por);
+            t.start();
+        }
     }
 }
+
